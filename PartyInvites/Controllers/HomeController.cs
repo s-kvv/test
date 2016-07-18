144c01d52bd4cecf376161b8b686707b721860e0
Krasnov Viacheslav <krasnovvv@ekassir.com>
2016-07-18 14:39:45 +0300

    Введение данных, вывод результата

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -24,5 +25,17 @@ namespace PartyInvites.Controllers
             return Redirect("https://ya.ru");
         }
 
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
+        }
     }
 }
