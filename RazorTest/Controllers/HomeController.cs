0104f7e79d2294405d624cf276be987c9253473c
Krasnov Viacheslav <krasnovvv@ekassir.com>
2016-07-19 18:45:50 +0300

    Start RazorTest ptoject

---
+++ RazorTest/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RazorTest.Models;
+
+namespace RazorTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+    }
+}
