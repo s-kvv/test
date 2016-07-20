0ae5e326f52931b9bc8497c76e3da41149956c81
Viacheslav Krasnov <s-kvv@ya.ru>
2016-07-20 22:45:41 +0300

    Dependensy Resolver

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using EssentialTools.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILinqValueCalculator _calc;
+
+        public HomeController(ILinqValueCalculator calc)
+        {
+            _calc = calc;
+        }
+
+        private Product[] products =
+        {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+            new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+            new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+        };
+
+        public ActionResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart(_calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+
+    }
+}
---
+++ EssentialTools/Models/ILinqValueCalculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EssentialTools.Models
+{
+    public interface ILinqValueCalculator
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+}
